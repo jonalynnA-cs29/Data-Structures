4e78caf48b55b1aaab5dddcd1a49348c3188746f
jonalynn masters <jonalynna.masters@gmail.com>
2020-05-11 17:44:00 -0700

    Queue with array implemented and passing all tests linked_list imports not working so logic is failing...even with making a linked_list file inside of the Queue folder

--- queue/queue.py
+++ queue/queue.py
@@ -1,28 +1,56 @@
+from linked_list import LinkedList
+
+
 """
 A queue is a data structure whose primary purpose is to store and
-return elements in First In First Out order. 
+return elements in First In First Out order.
 
 1. Implement the Queue class using an array as the underlying storage structure.
    Make sure the Queue tests pass.
 2. Re-implement the Queue class, this time using the linked list implementation
    as the underlying storage structure.
    Make sure the Queue tests pass.
-3. What is the difference between using an array vs. a linked list when 
+3. What is the difference between using an array vs. a linked list when
    implementing a Queue?
-   
+
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+
+"""Uncomment for array test"""
+"""array version"""
+# class Queue:
+
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def enqueue(self, value):
+#         self.storage.append(value)
+
+#     def dequeue(self):
+#         if len(self.storage) != 0:
+#             return self.storage.pop(0)
+
+"""Uncomment for SLL test"""
+"""SLL version"""
+
+
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
-    
+        self.storage = LinkedList()
+
     def __len__(self):
-        pass
+        self.size
 
     def enqueue(self, value):
-        pass
+        self.size += 1
+        return add_to_end(value)
 
     def dequeue(self):
-        pass
+        return remove_from_head(value)
+
